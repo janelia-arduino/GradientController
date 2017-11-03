996c9ecf2275c50568ca5b6f22a471148ad06016
Peter Polidoro <peterpolidoro@gmail.com>
2017-11-03 12:37:01 -0400

    Initial commit.

---
+++ examples/GradientController/GradientController.ino
@@ -0,0 +1,40 @@
+#include <Flash.h>
+#include <EEPROM.h>
+#include <Streaming.h>
+#include <ArduinoJson.h>
+#include <JsonSanitizer.h>
+#include <JsonStream.h>
+#include <Array.h>
+#include <Vector.h>
+#include <MemoryFree.h>
+#include <ConstantVariable.h>
+#include <SavedVariable.h>
+#include <Functor.h>
+#include <IndexedContainer.h>
+#include <FunctorCallbacks.h>
+
+#include <SPI.h>
+#include <TLE72X.h>
+#include <EventController.h>
+
+#include <ModularClient.h>
+
+#include <ModularServer.h>
+#include <ModularDeviceBase.h>
+#include <PowerSwitchController.h>
+#include <MixingValveController.h>
+#include <GradientController.h>
+
+
+GradientController dev;
+
+void setup()
+{
+  dev.setup();
+  dev.startServer();
+}
+
+void loop()
+{
+  dev.update();
+}
---
+++ src/GradientController.h
@@ -0,0 +1,55 @@
+// ----------------------------------------------------------------------------
+// GradientController.h
+//
+//
+// Authors:
+// Peter Polidoro polidorop@janelia.hhmi.org
+// ----------------------------------------------------------------------------
+#ifndef GRADIENT_CONTROLLER_H
+#define GRADIENT_CONTROLLER_H
+#include <Flash.h>
+#include <EEPROM.h>
+#include <Streaming.h>
+#include <ArduinoJson.h>
+#include <JsonSanitizer.h>
+#include <JsonStream.h>
+#include <Array.h>
+#include <Vector.h>
+#include <MemoryFree.h>
+#include <ConstantVariable.h>
+#include <SavedVariable.h>
+#include <Functor.h>
+#include <IndexedContainer.h>
+#include <FunctorCallbacks.h>
+
+#include <SPI.h>
+#include <TLE72X.h>
+#include <EventController.h>
+
+#include <ModularClient.h>
+
+#include <ModularServer.h>
+#include <ModularDeviceBase.h>
+#include <PowerSwitchController.h>
+#include <MixingValveController.h>
+
+#include "GradientController/Constants.h"
+
+
+class GradientController : public MixingValveController
+{
+public:
+  GradientController();
+  virtual void setup();
+
+private:
+  modular_server::Property properties_[gradient_controller::constants::PROPERTY_COUNT_MAX];
+  modular_server::Parameter parameters_[gradient_controller::constants::PARAMETER_COUNT_MAX];
+  modular_server::Function functions_[gradient_controller::constants::FUNCTION_COUNT_MAX];
+  modular_server::Callback callbacks_[gradient_controller::constants::CALLBACK_COUNT_MAX];
+
+  // Handlers
+
+};
+
+#endif
