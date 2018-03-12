a4efecb8bb0f100d79d2a4841f9f12ba4f2cb001
Peter Polidoro <peterpolidoro@gmail.com>
2018-03-12 12:50:23 -0400

    Added const ConstantString * to handler comments.

--- src/GradientController/GradientController.cpp
+++ src/GradientController/GradientController.cpp
@@ -157,6 +157,7 @@ bool GradientController::finishMix()
 // const char *
 // ArduinoJson::JsonArray *
 // ArduinoJson::JsonObject *
+// const ConstantString *
 //
 // For more info read about ArduinoJson parsing https://github.com/janelia-arduino/ArduinoJson
 //
