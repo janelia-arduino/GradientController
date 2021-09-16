4a5afa77c0e4d996b1662435b51dc110a730c0d1
Peter Polidoro <peterpolidoro@gmail.com>
2021-09-16 13:14:18 -0400

    Update email address

--- src/GradientController.h
+++ src/GradientController.h
@@ -3,7 +3,7 @@
 //
 //
 // Authors:
-// Peter Polidoro peterpolidoro@gmail.com
+// Peter Polidoro peter@polidoro.io
 // ----------------------------------------------------------------------------
 #ifndef GRADIENT_CONTROLLER_H
 #define GRADIENT_CONTROLLER_H
